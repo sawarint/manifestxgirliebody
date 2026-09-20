3c77350ca5e4eaf81e3d69552fdacd83d89e6a33
sawarint <115648393+sawarint@users.noreply.github.com>
2026-09-20 17:14:52 +0700

    Update eng version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Offline cache: serve the app shell from cache, refresh it in the background.
-const CACHE = "ladtee-v1";
-const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+const CACHE = "ladtee-v2";
+const SHELL = ["./", "./index.html", "./en.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
