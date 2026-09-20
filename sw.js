8eb2c33b637423c8f4b7e25ff270f0019c79b742
sawarint <115648393+sawarint@users.noreply.github.com>
2026-09-20 20:44:40 +0700

    Update new ui version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Pages: network first (always newest when online, cached copy when offline).
 // Other files: cache first, refreshed in the background.
-const CACHE = "ladtee-v10";
+const CACHE = "ladtee-v11";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", e => {
