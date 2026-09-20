97253072374a632ee41963fdf0cc9f4f4324beee
sawarint <115648393+sawarint@users.noreply.github.com>
2026-09-20 20:37:05 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Pages: network first (always newest when online, cached copy when offline).
 // Other files: cache first, refreshed in the background.
-const CACHE = "ladtee-v7";
-const SHELL = ["./", "./index.html", "./en.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+const CACHE = "ladtee-v10";
+const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
